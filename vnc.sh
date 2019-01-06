640783fc67d416cf90fe64d047fc4bfbe63f0eb9
berdbard <46423905+berdbard@users.noreply.github.com>
2019-01-07 01:40:23 +0700

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sudo yum groupinstall Desktop
-sudo yum install tigervnc-server
-sudo yum install xorg-x11-fonts-Type1
+sudo yum -y groupinstall Desktop
+sudo yum -y install tigervnc-server
+sudo yum -y install xorg-x11-fonts-Type1
 sudo chkconfig vncserver on
 vncpasswd
+
 service vncserver restart
 vncserver -kill :1
+
 service vncserver start
 service vncserver restart
