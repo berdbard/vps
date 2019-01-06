b0019c9790ee1dc822411f73e9dd7eadc164f4c7
berdbard <46423905+berdbard@users.noreply.github.com>
2019-01-07 02:14:20 +0700

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -6,6 +6,9 @@ sudo yum -y install xorg-x11-fonts-Type1
 sudo chkconfig vncserver on
 vncpasswd
 
+echo 'VNCSERVERS="1:root"' >> /etc/sysconfig/vncservers
+echo 'VNCSERVERARGS[1]="-geometry 1024×600"' >> /etc/sysconfig/vncservers
+
 service vncserver restart
 vncserver -kill :1
 
