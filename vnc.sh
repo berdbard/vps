a723f2ac44b1d3bcf5400370724dcad5cff779d9
berdbard <46423905+berdbard@users.noreply.github.com>
2019-01-07 01:29:17 +0700

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -5,10 +5,7 @@ sudo yum install tigervnc-server
 sudo yum install xorg-x11-fonts-Type1
 sudo chkconfig vncserver on
 vncpasswd
-echo "VNCSERVERS="1:root"" > /etc/sysconfig/vncservers
-echo "VNCSERVERARGS[1]="-geometry 1024×600"" > /etc/sysconfig/vncservers
 service vncserver restart
 vncserver -kill :1
-echo "exec gnome-session &" > .vnc/xstartup
 service vncserver start
 service vncserver restart
